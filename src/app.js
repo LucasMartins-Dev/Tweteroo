54c75295fda5a071ac8c15455f8e71211d4d9e1f
Lucas <lucasmartinsms.dev@gmail.com>
2023-01-08 12:46:53 -0300

    bonus

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
-import express  from "express";
+import express, {json} from "express";
 import cors from "cors";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(json())
 
 const tweteroos = []
 const usuarios = []
 
 
+
 app.post("/sign-up",(req,res)=>{
 const usuario = req.body
 usuarios.push(usuario)
+console.log(usuarios)
 res.send("ok")
 })
 
 app.post("/tweets",(req,res)=>{
-    const tweet = req.body
-    if(usuarios.find(usu => usu.username === tweet.username)){
+    const tweet ={username: req.headers.user,avatar: usuarios[0].avatar ,tweet: req.body.tweet}
+    
+    if(usuarios.find(usu => usu.username == tweet.username)){
         tweteroos.push(tweet)
         res.send("ok")
+      console.log(tweteroos)
     }else{
+        
         res.send("UNAUTHORIZED")
     }
 })
@@ -39,6 +45,7 @@ app.get("/tweets",(req,res)=>{
         return {username:d.username , avatar:usuario.avatar , tweet:d.tweet}
         })
     res.send(avatar)
+    
 })
 
 app.listen(5000,()=>console.log("ta funfando"))
